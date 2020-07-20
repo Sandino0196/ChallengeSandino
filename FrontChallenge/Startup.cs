bc0d70f91fa59589cffa43576dcfc1b7867c4bb4
Sandino <sandinorodrigo@gmail.com>
2020-07-20 11:45:55 -0600

    First commit (begin register)

---
+++ FrontChallenge/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FrontChallenge.Startup))]
+namespace FrontChallenge
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
