e72700bc6a55e2fa3219857c5c81148f05248546
Sandino <sandinorodrigo@gmail.com>
2020-07-20 13:28:34 -0600

    Database/register finish

--- ChallengeSandino/App_Start/WebApiConfig.cs
+++ ChallengeSandino/App_Start/WebApiConfig.cs
@@ -25,6 +25,8 @@ namespace ChallengeSandino
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.EnableCors();
         }
     }
 }
